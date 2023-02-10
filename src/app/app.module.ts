2166a7693b281e94324ad01cbd9361790e286148
Narayan <prema.narayan.ochoa@mi.unc.edu.ar>
2023-02-10 19:52:48 -0300

    Navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './components';
 
 @NgModule({
   declarations: [
@@ -11,7 +12,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ToolbarComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
