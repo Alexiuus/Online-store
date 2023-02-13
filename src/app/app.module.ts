cede2f83c8b4f4b16d1c665670e5d705645f98e0
Narayan <prema.narayan.ochoa@mi.unc.edu.ar>
2023-02-13 11:17:21 -0300

    Remove barrel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
-import { ToolbarComponent } from './components';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
