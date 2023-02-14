5932fcba0489fc26cfc17bd6534931d7b3df2520
Alexiuus <Alexis.centeno@mi.unc.edu.ar>
2023-02-14 16:48:39 -0300

    basic login structure

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,60 @@
+import { Component } from '@angular/core';
+import { FormControl, Validators, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+
+  /* registration form data */
+  formUser = new FormGroup({
+    username: new FormControl('', Validators.required),
+    password : new FormControl('', Validators.required)
+  });
+  
+  isRegister : boolean = false;
+  urlImagelike : string = 'https://static.vecteezy.com/system/resources/previews/010/142/101/original/check-mark-icon-sign-symbol-design-free-png.png';
+
+  get username(){
+    return this.formUser.get('username');
+  }
+
+  get password(){
+    return this.formUser.get('password');
+  }
+
+  isErrorPassword(password : any) : boolean {
+    const isPristinePassword : boolean = password?.pristine?? false;
+
+    return !(isPristinePassword) && this.isEmptyPassword(password);
+  }
+  private isEmptyPassword(password : any) : boolean {
+    const isPasswordRequired : boolean = password?.errors?.['required']?? false;
+
+    return isPasswordRequired ;
+  }
+  
+  getErrorPassword(password : any) : String {
+    if (this.isEmptyPassword(password)) return 'Empty passsword';
+  
+    return '';
+  }
+
+  isErrorUsername(username : any) : boolean {
+    return username?.valid || username?.pristine;
+  }
+
+  getErrorUsername(username : any) : string {
+    if(username?.errors?.['required']) return 'empty username';
+    else if(username?.errors?.['username']) return 'invalid username';
+
+    return '';
+  }
+
+  sendDataRegister(){
+    console.log("Registrando")
+    this.isRegister = true;
+  }
+}
