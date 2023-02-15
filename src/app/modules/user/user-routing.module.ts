e9a809a0e6ce5d194c4460531be7790a3925258b
Alexiuus <Alexis.centeno@mi.unc.edu.ar>
2023-02-15 19:20:23 -0300

    changes and modifications in login and register

--- src/app/modules/user/user-routing.module.ts
+++ src/app/modules/user/user-routing.module.ts
@@ -1,7 +1,7 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from 'src/app/components/login/login.component';
-import { RegisterComponent } from 'src/app/components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 import { UserComponent } from './user.component';
 
 const routes: Routes = [
--- src/app/modules/user/user.module.ts
+++ src/app/modules/user/user.module.ts
@@ -4,11 +4,12 @@ import { UserComponent } from './user.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { UserRoutingModule } from './user-routing.module';
 import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
-import { RegisterComponent } from 'src/app/components/register/register.component';
 import {MatToolbarModule} from '@angular/material/toolbar'; 
 import {MatIconModule} from '@angular/material/icon'; 
 import {MatButtonModule} from '@angular/material/button'; 
-import { LoginComponent } from 'src/app/components/login/login.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+
 @NgModule({
   declarations: [
     UserComponent,
