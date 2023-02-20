6a8f74bfa7f327234231bc1507e8c8bcf24beb06
Alexiuus <Alexis.centeno@mi.unc.edu.ar>
2023-02-20 16:03:47 -0300

    adding observable

--- src/app/modules/product/product.component.ts
+++ src/app/modules/product/product.component.ts
@@ -1,3 +1,4 @@
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Component, HostListener, OnInit } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
 import { ProductsExampleList, Product } from '../home/example/ProductsExample';
@@ -10,18 +11,21 @@ import { ProductsExampleList, Product } from '../home/example/ProductsExample';
 export class ProductComponent implements OnInit {
   private id_product : number = 0;
   product : Product = ProductsExampleList[0];
-  isMobile = window.innerWidth <= 768;
+  isMobile : boolean = false;
 
-  @HostListener('window:resize', ['$event'])
-  onResize(event: any) {
-    this.isMobile = event.target.innerWidth <= 768;
-  }
-  constructor(private _route:ActivatedRoute) { }
+  constructor(private _route:ActivatedRoute, private windowObserver: BreakpointObserver) { }
 
   ngOnInit() {
+    /* route parameters */
     this._route.params.subscribe((params : Params) =>{
       this.id_product = parseInt(params['id_product']) ;
       this.product = ProductsExampleList.find(elem => elem.id === this.id_product)?? ProductsExampleList[0];
     });
+    
+    /* check if the screen is mobile */
+    const customQuery = '(max-width: 768px)';
+    this.windowObserver.observe([customQuery]).subscribe(
+      result => this.isMobile = result.matches
+    )
   }
 }
