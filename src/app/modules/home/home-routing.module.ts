fa7cad97862d7337f96892295d75eae4d3bdb220
Narayan <prema.narayan.ochoa@mi.unc.edu.ar>
2023-02-21 09:17:58 -0300

    NavBar with products

--- src/app/modules/home/home-routing.module.ts
+++ src/app/modules/home/home-routing.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ProductComponent } from './components/product/product.component';
 import { HomeComponent } from './home.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: HomeComponent
-  },
-  {
-    path: 'product/:id_product',
-    component: ProductComponent
+    component: HomeComponent,
+    children: [
+      {
+        path: 'product/:id_product',
+        component: ProductComponent
+      },
+      {
+        path: '',
+        component: DashboardComponent
+      }
+    ]
   }
 ];
 
